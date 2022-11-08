95c40f0fabaa84e540facb2b4b9d9a25259c8b88
huchun <huchun@abc.com>
2022-11-08 09:06:48 +0000

    done:fim

--- src/pages/fim/components/Category.tsx
+++ src/pages/fim/components/Category.tsx
@@ -9,11 +9,12 @@ import styles from "styles/Fim.module.scss";
 //解决方案
 export default function Category() {
   return (
-    <div className="bg-rev-bg-black-0B pb-32">
+    <div className="bg-rev-bg-black-0B pb-32 relative">
       <div className="container">
         <h2 className="section-title">FIM的优势</h2>
         <FimRing />
       </div>
+      <div className={styles["category-bg"]}></div>
     </div>
   );
 }
