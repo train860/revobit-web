a33a406b8ee24ae02da7a851037ab31aa9d1ad70
huchun <huchun@abc.com>
2022-11-24 05:19:00 +0000

    更换模型加载方式

--- src/views/home/Shoe.tsx
+++ src/views/home/Shoe.tsx
@@ -1,3 +1,11 @@
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace JSX {
+    interface IntrinsicElements {
+      "model-viewer": unknown;
+    }
+  }
+}
 const Shoe = () => {
   return (
     <model-viewer
